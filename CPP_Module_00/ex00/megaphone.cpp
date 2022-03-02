51304784bb71a3de3eb99d051a05a633b416d7d0
wleite <wleite@student.42sp.org.br>
2022-03-02 03:45:59 -0300

    feat: added CPP_Module_00/ex00

---
+++ CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < argc; i++) {
+		std::string str(argv[i]);
+		for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
+			char c = toupper(*it);
+			std::cout << c;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
