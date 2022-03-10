a9669098e29a850557e4cdef5b0d3b3d921fe8fc
wleite <wleite@student.42sp.org.br>
2022-03-10 04:06:30 -0300

    refactor: restored main in CPP_Module_02/ex02

--- CPP_Module_02/ex02/main.cpp
+++ CPP_Module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wleite <wleite@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 05:23:37 by wleite            #+#    #+#             */
-/*   Updated: 2022/03/10 03:31:05 by wleite           ###   ########.fr       */
+/*   Updated: 2022/03/10 04:02:16 by wleite           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@ int main(void) {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
 
-	std::cout << "1 " << a << std::endl;
-	std::cout << "2 " << ++a << std::endl;
-	std::cout << "3 " << a << std::endl;
-	std::cout << "4 " << a++ << std::endl;
-	std::cout << "5 " << a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 
-	std::cout << "6 " << b << std::endl;
+	std::cout << b << std::endl;
 
-	std::cout << "7 " << Fixed::max(a, b) << std::endl;
-
+	std::cout << Fixed::max(a, b) << std::endl;
 	return (0);
 }
