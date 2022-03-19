962e77d2fd00ca296264bfbda0a98f9dd836b13e
wleite <wleite@student.42sp.org.br>
2022-03-19 20:30:28 -0300

    feat: added CPP_Module_07/ex01

---
+++ CPP_Module_07/ex01/iter.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Welton Leite, wleite. All rights reserved.
+
+#ifndef EX01_ITER_HPP_
+#define EX01_ITER_HPP_
+
+template <typename T>
+void iter(T* addr, size_t lenght, void (*f)(const T&)) {
+  for (size_t i = 0; i < lenght; i++) {
+    f(addr[i]);
+  }
+}
+
+template <typename T>
+void print(const T& x) {
+  std::cout << x << " ";
+}
+
+#endif  // EX01_ITER_HPP_
---
+++ CPP_Module_07/ex01/main.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2022 Welton Leite, wleite. All rights reserved.
+
+#include <iostream>
+
+#include "iter.hpp"
+
+int main(void) {
+  int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  char arr2[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+  float arr3[10] = {0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
+
+  std::cout << "[ INT ARRAY ]\n";
+  iter(arr, 10, print);
+  std::cout << "\n";
+
+  std::cout << "\n";
+
+  std::cout << "[ CHAR ARRAY ]\n";
+  iter(arr2, 10, print);
+  std::cout << "\n";
+
+  std::cout << "\n";
+
+  std::cout << "[ FLOAT ARRAY ]\n";
+  iter(arr3, 10, print);
+  std::cout << "\n";
+
+  return (0);
+}
