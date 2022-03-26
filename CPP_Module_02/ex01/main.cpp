f09f2f0d0011d91f4d0ec8623f46eec678cf1337
wleite <wleite@student.42sp.org.br>
2022-03-26 18:43:05 -0300

    test: added new tests in CPP_Module_03

--- CPP_Module_02/ex01/main.cpp
+++ CPP_Module_02/ex01/main.cpp
@@ -12,15 +12,25 @@ int main(void) {
 
   a = Fixed(1234.4321f);
 
+  std::cout << "\n[ DEFAULT ]" << std::endl;
   std::cout << "a is " << a << std::endl;
   std::cout << "b is " << b << std::endl;
   std::cout << "c is " << c << std::endl;
   std::cout << "d is " << d << std::endl;
 
+  std::cout << "\n[ TO INT ]" << std::endl;
   std::cout << "a is " << a.toInt() << " as integer" << std::endl;
   std::cout << "b is " << b.toInt() << " as integer" << std::endl;
   std::cout << "c is " << c.toInt() << " as integer" << std::endl;
   std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+  std::cout << "\n[ TO FLOAT ]" << std::endl;
+  std::cout << "a is " << a.toFloat() << " as float" << std::endl;
+  std::cout << "b is " << b.toFloat() << " as float" << std::endl;
+  std::cout << "c is " << c.toFloat() << " as float" << std::endl;
+  std::cout << "d is " << d.toFloat() << " as float" << std::endl;
+
+  std::cout << std::endl;
 
   return (0);
 }
--- CPP_Module_02/ex02/main.cpp
+++ CPP_Module_02/ex02/main.cpp
@@ -8,6 +8,7 @@ int main(void) {
   Fixed a;
   Fixed const b(Fixed(5.05f) * Fixed(2));
 
+  std::cout << "\n[ PDF Test ]" << std::endl;
   std::cout << a << std::endl;
   std::cout << ++a << std::endl;
   std::cout << a << std::endl;
@@ -17,5 +18,40 @@ int main(void) {
   std::cout << b << std::endl;
 
   std::cout << Fixed::max(a, b) << std::endl;
+
+  Fixed c(1);
+  Fixed d(2);
+  std::cout << "\n[ Comparison operator test ]" << std::endl;
+  std::cout << "c = " << c << " | " << "d = " << d << std::endl;
+  std::cout << "\n[ c  > d ]" << " result: " << (c > d);
+  std::cout << "\n[ c  < d ]" << " result: " << (c < d);
+  std::cout << "\n[ c >= d ]" << " result: " << (c >= d);
+  std::cout << "\n[ c <= d ]" << " result: " << (c <= d);
+  std::cout << "\n[ c == d ]" << " result: " << (c == d);
+  std::cout << "\n[ c != d ]" << " result: " << (c != d) << std::endl;
+
+  c = Fixed(2);
+  d = Fixed(4);
+  std::cout << "\n[ Math operator test ]" << std::endl;
+  std::cout << "c = " << c << " | " << "d = " << d << std::endl;
+  std::cout << "\n[ c + d ]" << " result: " << (c + d);
+  std::cout << "\n[ c - d ]" << " result: " << (c - d);
+  std::cout << "\n[ c * d ]" << " result: " << (c * d);
+  std::cout << "\n[ c / d ]" << " result: " << (c / d) << std::endl;
+
+  c = Fixed(1);
+  std::cout << "\n[ Increment/Deprecrement test ]" << std::endl;
+  std::cout << "c = " << c << std::endl;
+  std::cout << "\n[ c++ ]" << " result: " << (c++);
+  std::cout << "\n[ ++c ]" << " result: " << (++c);
+  std::cout << "\n[ c-- ]" << " result: " << (c--);
+  std::cout << "\n[ --c ]" << " result: " << (--c) << std::endl;
+
+  c = Fixed(10);
+  d = Fixed(20);
+  std::cout << "\n[ Min/Max test ]" << std::endl;
+  std::cout << "c = " << c << " | " << "d = " << d << std::endl;
+  std::cout << "\n[ min ]" << " result: " << Fixed::min(c, d);
+  std::cout << "\n[ max ]" << " result: " << Fixed::max(c, d) << std::endl;
   return (0);
 }
